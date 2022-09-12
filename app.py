064d51a6e7a0824019eca5a48ed96c7d9715a113
alexfirestone <alexdfirestone@gmail.com>
2022-09-11 23:46:43 -0400

    add user chart

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import json
+from datetime import datetime as dt
 from api_lib import call_ugig_api
 
 
@@ -17,3 +18,15 @@ users_df = pd.DataFrame(fetch_user_response['users'])
 st.subheader('Users')
 st.write(f"Total Users:  {fetch_user_response['count']}")
 st.dataframe(users_df)
+
+accounts_created_df = users_df[['UserCreateDate', 'UserStatus']].copy()
+def convert_date_strings(x):
+    sub_str = x[0:10]
+    format = "%Y-%m-%d"
+    date = dt.strptime(sub_str, format)
+    return date
+accounts_created_df['AccountCreatedDate'] = accounts_created_df['UserCreateDate'].apply(convert_date_strings)
+
+accounts_created_df = accounts_created_df['AccountCreatedDate'].value_counts().rename_axis('Date').reset_index(name='New Users')
+st.bar_chart(data=accounts_created_df, x='Date', y='New Users')
+
